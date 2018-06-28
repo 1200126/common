b6c302dbebf82f3bd78b30ecdfca7fe887108c2d
1200126 <1200126@qq.com>
2018-06-28 13:15:31 +0800

    新建项目

---
+++ Common/src/main/java/com/hzy/common/utils/KeybordS.java
@@ -0,0 +1,80 @@
+package com.hzy.common.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+/**
+ * 类名: KeybordS</br>
+ * 包名：com.hzy.common.utils </br>
+ * 描述: </br>
+ * 发布版本号：</br>
+ * 开发人员： huangzy</br>
+ * 创建时间： 2018/4/18 下午3:09 </br>
+ */
+public class KeybordS {
+
+    /**
+     * 打开软键盘
+     *
+     * @param mEditText
+     * @param mContext
+     */
+    public static void openKeybord(EditText mEditText, Context mContext) {
+        InputMethodManager imm = (InputMethodManager) mContext
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(mEditText, InputMethodManager.RESULT_SHOWN);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,
+                InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    /**
+     * 关闭软键盘
+     *
+     * @param mEditText 输入框
+     * @param mContext 上下文
+     */
+    public static void closeKeybord(EditText mEditText, Context mContext) {
+        InputMethodManager imm = (InputMethodManager) mContext
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+    }
+
+    /**
+     * 判断当前软键盘是否打开
+     *
+     * @param activity
+     * @return
+     */
+    public static boolean isSoftInputShow(Activity activity) {
+
+        // 虚拟键盘隐藏 判断view是否为空
+        View view = activity.getWindow().peekDecorView();
+        if (view != null) {
+            // 隐藏虚拟键盘
+            InputMethodManager inputmanger = (InputMethodManager) activity
+                    .getSystemService(Activity.INPUT_METHOD_SERVICE);
+            //       inputmanger.hideSoftInputFromWindow(view.getWindowToken(),0);
+
+            return inputmanger.isActive() && activity.getWindow().getCurrentFocus() != null;
+        }
+        return false;
+    }
+
+
+    /**
+     * 详述:  强制隐藏软键盘</br>
+     * 开发人员：huangzy</br>
+     * 创建时间：2018/5/11 上午11:26 </br>
+     * @param
+     */
+    public static void KeyBoardCancle(Activity activity) {
+        View view = activity.getWindow().peekDecorView();
+        if (view != null) {
+            InputMethodManager inputmanger = (InputMethodManager) activity.getSystemService(activity.INPUT_METHOD_SERVICE);
+            inputmanger.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+}
